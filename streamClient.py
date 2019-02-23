1b69597e0d129d6c261e41c45c770179e8181c9f
scottbarnesg <scottgbarnes@gwu.edu>
2019-02-23 15:36:17 -0500

    Bug fixes, improved stability

--- streamClient.py
+++ streamClient.py
@@ -7,7 +7,7 @@ class Streamer():
 	def __init__(self, path, queueSize=256):
 		self.stream = cv2.VideoCapture(path)
 		self.Q = Queue(maxsize=queueSize)
-		self.delay = 100
+		self.delay = 50
 		self.sleepTime = 0.01
 
 	def read(self):
@@ -17,11 +17,14 @@ class Streamer():
 		return self.Q.qsize() > 0
 
 	def update(self):
-			print self.Q.qsize()
+		while True:
+			# print self.Q.qsize()
 			if not self.Q.full():
-				(grabbed, frame) = self.stream.read()
-				if grabbed:
+				ret, frame = self.stream.read()
+				if ret:
 					self.Q.put(frame)
+			else:
+				time.sleep(self.sleepTime)
 
 	def dynamicUpdate(self):
 		if self.Q.qsize() < 50:
@@ -34,12 +37,12 @@ class Streamer():
 		if self.Q.qsize() > 100 and self.delay > 90:
 			self.delay -= 1
 		elif self.Q.qsize() < 100:
-			self.delay += 5
+			self.delay += 1
 			self.update()
 
 	def render(self):
 		while True:
-			self.dynamicFPS()
+			# self.dynamicFPS()
 			if self.QNotEmpty():
 				frame = self.read()
 				cv2.imshow("image", frame)
@@ -53,11 +56,11 @@ class Streamer():
 		renderThread = Thread(target = self.render)
 
 		updaterThread.start()
-		time.sleep(1)
+		time.sleep(2)
 		renderThread.start()
 
 
 if __name__ == '__main__':
-	path = '/home/scott/smart_sec_cam/bin/videos12/recording0.avi'
+	path = '/home/scott/smart_sec_cam/bin/videos17/recording0.avi'
 	streamer = Streamer(path)
 	streamer.run()
