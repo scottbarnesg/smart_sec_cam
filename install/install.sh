8a189dd4ecc0be69e0ed5eb1ec385b56dd34fbf1
scottbarnesg <scottgbarnes@gwu.edu>
2019-06-16 21:13:05 -0400

    System service prototype

--- install/install.sh
+++ install/install.sh
@@ -12,14 +12,28 @@ python -m pip install opencv-python
 python -m pip install flask
 python -m pip install requests
 
-echo 'Is this a server instance (y/n)?'
+echo -n 'Is this a server instance (y/n)? '
 read is_server
 if [ "${is_server,,}" == "y" ]; then
   echo "Generating SSL certificate"
   bash generate-cert.sh
-  echo "Done"
+  echo "Creating system service"
+  path=$(cd ../ && pwd)
+  sed -i "/^User=/ s/$/$USER/" ../service/sec-cam-server.service
+  echo "ExecStart=/usr/bin/python $path/streamServer.py" >> ../service/sec-cam-server.service
+  echo "WorkingDirectory=$path/" >> ../service/sec-cam-server.service
+  sudo cp ../service/sec-cam-server.service /etc/systemd/system/
+  echo -n "Would you like the server to run automatically (y/n)? "
+  read create_service
+  if [ "${create_service,,}" == "y" ]; then
+    echo "Enabling and starting server service"
+    sudo systemctl enable sec-cam-server
+    sudo systemctl start sec-cam-server
+  fi
 fi
 
 echo 'Install complete!'
+echo 'To start the server service: sudo systemctl start sec-cam-server'
 echo 'To run the server: python smart_sec_cam/streamServer.py'
 echo 'To run the client: python smart_sec_cam/streamClient.py'
+cd ..
